a724fe85eba7bed4555faf885b536af40d2fc8e4
plastikron <thyenkay@dv-schulen.de>
2018-04-18 11:54:21 +0200

    läuft

---
+++ Übung3WPF/Person.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Übung3WPF
+{
+    class Person
+    {
+        private String vorname;
+        private String nachname;
+        private int alter;
+        private String haustier;
+
+        public Person()
+        {
+
+        }
+        public Person(String vorname, String nachname)
+        {
+            Vorname = vorname;
+            Nachname = nachname;
+        }
+
+        public string Vorname
+        {
+            get
+            {
+                return vorname;
+            }
+
+            set
+            {
+                vorname = value;
+            }
+        }
+
+        public string Nachname
+        {
+            get
+            {
+                return nachname;
+            }
+
+            set
+            {
+                nachname = value;
+            }
+        }
+
+        public int Alter
+        {
+            get
+            {
+                return alter;
+            }
+        }
+        public string Haustier
+        {
+            get
+            {
+                return haustier;
+            }
+
+            set
+            {
+                haustier = value;
+            }
+        }
+
+        public void Geburtstag()
+        {
+
+        }
+    }
+}
